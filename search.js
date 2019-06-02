144b4ad1918f9cbfdf6d233101d202041065d07d
Gabriel Almeida <galmeida9@gmail.com>
2019-06-02 21:30:11 +0100

    added search on click

--- search.js
+++ search.js
@@ -1,7 +1,8 @@
+var noEnter = 0;
+
 function handleQuery(a, b) {
     var c = a.keyCode || a.which;
-    if (13 === c) {
-        console.log(b[0]);
+    if ((13 === c) || noEnter == 1) {
         if (b[0] == "!" && b[1] == "g") {
             document.getElementsByClassName("search-txt")[0].value = b.substr(3);
             document.forms["searchform"].submit(); 
@@ -12,7 +13,26 @@ function handleQuery(a, b) {
             site += document.getElementsByClassName("search-txt")[0].value;
             window.open(site);
         }
+        else if (b[0] == "!" && b[1] == "y") {
+            document.getElementsByClassName("search-txt")[0].value = b.substr(3);
+            var site = "https://www.youtube.com/results?search_query=";
+            site += document.getElementsByClassName("search-txt")[0].value;
+            window.open(site);
+        }
+        else if (b[0] == "!" && b[1] == "t") {
+            document.getElementsByClassName("search-txt")[0].value = b.substr(3);
+            var site = "https://twitter.com/search?q=";
+            site += document.getElementsByClassName("search-txt")[0].value;
+            window.open(site);
+        }
         else document.forms["searchform"].submit(); 
     }
+    noEnter = 0;
            
+}
+
+function searchPress(a) {
+    noEnter = 1;
+    console.log(document.getElementsByClassName("search-txt")[0].value);
+    handleQuery(a, document.getElementsByClassName("search-txt")[0].value);
 }
