e36cff47cfb9a471f04ebcbae7f5f26489cad592
Gabriel Almeida <galmeida9@gmail.com>
2019-06-02 14:19:24 +0100

    added weather and time

---
+++ clock.js
@@ -0,0 +1,28 @@
+var timeout;
+clock();
+displayDate();
+
+function clock() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+
+    document.getElementById("hour").innerHTML = hours + ":" + minutes;
+
+    timeout = setTimeout(clock, 1000);
+}
+
+function displayDate() {
+    var date = new Date();
+    day = date.getDate();
+    month = date.getMonth() + 1;
+    year = date.getFullYear();
+
+    if (day < 10) day = "0" + day;
+    if (month < 10) month = "0" + month;
+
+    document.getElementById("date").innerHTML = day + "/" + month + "/" + year;
+}
---
+++ weather.js
@@ -0,0 +1,30 @@
+getLocation();
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(weather);
+    } else { 
+      //x.innerHTML = "Geolocation is not supported by this browser.";
+      console.log("error");
+    }
+}
+
+function weather(position){
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?lat='+position.coords.latitude+
+    '&lon='+position.coords.longitude+'&APPID=43781a0c36823440a1bc53550259e697&units=metric', true);
+
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+        console.log(data);
+        if (request.status >= 200 && request.status < 400) {
+            document.getElementById("weatherCity").innerHTML = "シティ: " + data["city"]["name"] + ", " + data["city"]["country"];
+            document.getElementById("weatherTemp").innerHTML = "天気: " + Math.ceil(data["list"]["0"]["main"]["temp"]) + "°C";
+            document.getElementById("weatherCondition").innerHTML = data["list"]["0"]["weather"]["0"]["main"];
+            document.getElementById("weatherImg").src = "https://openweathermap.org/img/w/" + data["list"]["0"]["weather"]["0"]["icon"] + ".png";
+        } 
+        else {console.log('error')}
+    }
+    request.send();
+}
