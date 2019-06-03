78cdf822340157472d9eb7a58d02c10fd5430af5
Gabriel Almeida <galmeida9@gmail.com>
2019-06-03 01:51:15 +0100

    added game search

--- search.js
+++ search.js
@@ -2,12 +2,8 @@ var noEnter = 0;
 
 function handleQuery(a, b) {
     var c = a.keyCode || a.which;
-    if ((13 === c) || noEnter == 1) {
-        if (b[0] == "!" && b[1] == "g") {
-            document.getElementsByClassName("search-txt")[0].value = b.substr(3);
-            document.forms["searchform"].submit(); 
-        }  
-        else if (b[0] == "!" && b[1] == "r") {
+    if ((13 === c) || noEnter == 1) { 
+        if (b[0] == "!" && b[1] == "r") {
             document.getElementsByClassName("search-txt")[0].value = b.substr(3);
             var site = "https://www.reddit.com/search/?q=";
             site += document.getElementsByClassName("search-txt")[0].value;
@@ -34,6 +30,12 @@ function handleQuery(a, b) {
         else if (b[0] == "!" && b[1] == "f") {
             document.getElementsByClassName("search-txt")[0].value = b.substr(3);
             var site = "https://www.facebook.com/search/top/?q=";
+            site += document.getElementsByClassName("search-txt")[0].value;
+            window.open(site,"_self");
+        }
+        else if (b[0] == "!" && b[1] == "g") {
+            document.getElementsByClassName("search-txt")[0].value = b.substr(3);
+            var site = "https://igg-games.com/?s=";
             site += document.getElementsByClassName("search-txt")[0].value;
             window.open(site,"_self");
         }
