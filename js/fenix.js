9d3c1fd98bf4c26c6c32bc595e06d5343b18ff12
Gabriel Almeida <galmeida9@gmail.com>
2019-06-04 01:41:27 +0100

    added anime popup

--- js/fenix.js
+++ js/fenix.js
@@ -4,12 +4,24 @@ function openFenix() {
     document.body.style.overflow = "hidden";
 }
 
-window.onclick = function(event) {
-    var popup = document.getElementById("fenixpopup");
+function openAnime() {
+    document.getElementById("animepopup").style.visibility = "visible";
+    document.getElementById("animepopup").style.opacity = "1";
+    document.body.style.overflow = "hidden";
+}
 
-    if (event.target == popup) {
+window.onclick = function(event) {
+    var fenix = document.getElementById("fenixpopup");
+    var anime = document.getElementById("animepopup");
+
+    if (event.target == fenix) {
         document.getElementById("fenixpopup").style.visibility = "hidden";
         document.getElementById("fenixpopup").style.opacity = "0";
+        document.body.style.overflow = "scroll";
+    }
+    else if (event.target == anime) {
+        document.getElementById("animepopup").style.visibility = "hidden";
+        document.getElementById("animepopup").style.opacity = "0";
         document.body.style.overflow = "scroll";
     }
 }
